a5316dbcd17ebad5f43a5b266a76d5f50ccf2615
Nóra Szepes <lordblendi@gmail.com>
2018-02-07 20:05:04 +0100

    returning a promise for authenticator #111

--- app/authenticators/shibboleth.js
+++ app/authenticators/shibboleth.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 import config from '../config/environment';
+import RSVP from 'rsvp';
 
 export default Base.extend({
   ajax: Ember.inject.service(),
   session: Ember.inject.service('session'),
 
   authenticate() {
-    var url = config['ember-simple-auth-token'].serverTokenShibbolethEndpoint;
-    return this.get('ajax').request(url, {
-      type: 'GET',
-      dataType: 'json',
-      headers: {
-        'Content-Type': 'application/vnd.api+json'
-      }
-    }).then((response) => {
-      console.log(response);
+    return new RSVP.Promise(function(resolve, reject){
+      return this.get('ajax').request(config['ember-simple-auth-token'].serverTokenShibbolethEndpoint, {
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+          'Content-Type': 'application/vnd.api+json'
+        }
+      });
     });
   }
 });

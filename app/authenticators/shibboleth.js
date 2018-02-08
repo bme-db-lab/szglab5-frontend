dab50a1b6011fbfb5365f6229b7c586b92f33f3f
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-08 11:49:33 +0100

    Fixes #111 as backend returns json now

--- app/authenticators/shibboleth.js
+++ app/authenticators/shibboleth.js
@@ -1,21 +1,11 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 import config from '../config/environment';
-import RSVP from 'rsvp';
 
 export default Base.extend({
   ajax: Ember.inject.service(),
-  session: Ember.inject.service('session'),
 
   authenticate() {
-    return new RSVP.Promise(function(resolve, reject){
-      return this.get('ajax').request(config['ember-simple-auth-token'].serverTokenShibbolethEndpoint, {
-        type: 'GET',
-        dataType: 'json',
-        headers: {
-          'Content-Type': 'application/vnd.api+json'
-        }
-      });
-    });
+    return this.get('ajax').request(config['ember-simple-auth-token'].serverTokenShibbolethEndpoint);
   }
 });
--- app/routes/login-shibboleth.js
+++ app/routes/login-shibboleth.js
@@ -6,8 +6,15 @@ export default Ember.Route.extend(ErrorRouteMixin,  {
   session: Ember.inject.service('session'),
   beforeModel() {
     this._super(...arguments);
-    var session = this.get('session').authenticate('authenticator:shibboleth').catch((e) => {
-        this.transitionTo('login', { queryParams: { errorMessage: "Shibboleth error: " + e['errors'][0]['title'] }});
+    var session = this.get('session').authenticate('authenticator:shibboleth').then(()=>{
+      this.transitionTo('settings');
+    }).catch((e) => {
+        if ((e['errors'] !== undefined) && (e['errors'].length > 0) && (e['errors'][0]['title'] !== undefined)) {
+          this.transitionTo('login', { queryParams: { errorMessage: "Shibboleth login error: " + e['errors'][0]['title'] }});
+        }
+        else {
+          this.transitionTo('login', { queryParams: { errorMessage: "Shibboleth login error"}});
+        }
     });
   }
 });

ae30df3c19b4cd6907a2a400b09414961dce54ba
Nóra Szepes <lordblendi@gmail.com>
2018-02-18 22:18:06 +0100

    fix getting redirect parameter -> not this.

--- app/routes/login-shibboleth.js
+++ app/routes/login-shibboleth.js
@@ -7,14 +7,13 @@ export default Ember.Route.extend(ErrorRouteMixin,  {
   beforeModel() {
     this._super(...arguments);
 
-    var params = this.paramsFor('login-shibboleth');
-    console.log(params);
+    var redirect = this.paramsFor('login-shibboleth')['redirect'];
     var session = this.get('session').authenticate('authenticator:shibboleth').then(()=>{
-      if (this.get('redirect') === null) {
+      if (redirect === null) {
         this.sendAction('goToSettings');
       }
       else {
-        window.location.replace(`${decodeURIComponent(params.get('redirect'))}&token=${this.get('session.data.authenticated.token')}`);
+        window.location.replace(`${decodeURIComponent(redirect)}&token=${this.get('session.data.authenticated.token')}`);
       }
     }).catch((e) => {
         if ((e['errors'] !== undefined) && (e['errors'].length > 0) && (e['errors'][0]['title'] !== undefined)) {

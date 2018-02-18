e0624dae35c9a49db88838ec90b04a399ec5b5c7
Nóra Szepes <lordblendi@gmail.com>
2018-02-18 22:13:41 +0100

    Redirect is a query param

--- app/routes/login-shibboleth.js
+++ app/routes/login-shibboleth.js
@@ -6,12 +6,15 @@ export default Ember.Route.extend(ErrorRouteMixin,  {
   session: Ember.inject.service('session'),
   beforeModel() {
     this._super(...arguments);
+
+    var params = this.paramsFor('login-shibboleth');
+    console.log(params);
     var session = this.get('session').authenticate('authenticator:shibboleth').then(()=>{
       if (this.get('redirect') === null) {
         this.sendAction('goToSettings');
       }
       else {
-        window.location.replace(`${decodeURIComponent(this.get('redirect'))}&token=${this.get('session.data.authenticated.token')}`);
+        window.location.replace(`${decodeURIComponent(params.get('redirect'))}&token=${this.get('session.data.authenticated.token')}`);
       }
     }).catch((e) => {
         if ((e['errors'] !== undefined) && (e['errors'].length > 0) && (e['errors'][0]['title'] !== undefined)) {

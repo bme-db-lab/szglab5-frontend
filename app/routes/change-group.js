7bddf7de5af6914e82ca2bf1ee55204e485c5691
Nóra Szepes <nora.szepes@tenforce.com>
2018-08-30 18:15:21 +0200

    Change-group: get eventTemplates and studentroups in the model

--- app/routes/change-group.js
+++ app/routes/change-group.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import AdminAuthenticatedRouteMixin from '../mixins/admin-authenticated-route';
+import ErrorRouteMixin from '../mixins/error-route';
+import config from '../config/environment';
 
-export default Ember.Route.extend({
+import DemonstratorAuthenticatedRouteMixin from '../mixins/demonstrator-authenticated-route';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin,  DemonstratorAuthenticatedRouteMixin, AdminAuthenticatedRouteMixin, ErrorRouteMixin, {
+  model() {
+    let eventTemplates =  Ember.$.ajax({
+      type: "GET",
+      url: `${config.backendUrl}/event-templates-simple`,
+      beforeSend: (xhr) => { xhr.setRequestHeader('Authorization', `Bearer ${this.get('session.data.authenticated.token')}`); },
+      contentType: "application/json; charset=utf-8",
+      crossDomain: true,
+      dataType: "json",
+      success: (eventTemplates) => {
+        return eventTemplates;
+      }
+    });
+
+    return RSVP.hash({
+        studentGroups: this.get('store').findAll('studentGroup'),
+        eventTemplates: eventTemplates
+    });
+  }
 });

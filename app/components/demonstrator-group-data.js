4c7dbc093a264f61a44215b2449430efdf9598db
Szepes Nóra <nora.szepes@tenforce.com>
2020-10-01 19:37:35 +0200

    calculate event data in demostrator group data

--- app/components/demonstrator-group-data.js
+++ app/components/demonstrator-group-data.js
@@ -7,9 +7,32 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
   session: Ember.inject.service('session'),
   classNames: ['demonstrator-group'],
+
   header: ['Név', 'Neptun', "Csoport", 'Feladattípus', 'Feltöltés ideje', 'Beugró érdemjegy', 'Beadandó érdemjegy', 'Labor érdemjegy', 'Pót'],
   rowIndecies: ['StudentRegistration.User.displayName', 'StudentRegistration.User.neptun', 'StudentRegistration.neptunCourseCode', 'ExerciseSheet.ExerciseType.shortName', 'firstCorrectableDeliverable.formattedLastSubmittedDate', 'firstEntryTest.grade', 'firstCorrectableDeliverable.grade', 'grade', 'supplementary'],
 
+  eventGroup: Ember.computed('events', function(){
+    return this.get('events').then (function(events){
+      return events.get('firstObject.StudentRegistration.neptunCourseCode');
+    });
+  }),
+  eventPlace: Ember.computed('events', function(){
+    return this.get('events').then (function(events){
+      return events.get('firstObject.location');
+    });
+  }),
+  eventDate: Ember.computed('events', function(){
+    return this.get('events').then (function(events){
+      var date = events.get('firstObject.date');
+      return date;
+    });
+  }),
+  eventDeadline: Ember.computed('events', function(){
+    return this.get('events').then (function(events){
+      var date = events.get('firstObject.eventDeadline');
+      return date;
+    });
+  }),
 
   actions: {
     evaluateEvent(event) {

d6c2093c10be83d67b550cd38011939c59cab353
Szepes Nóra <nora.szepes@tenforce.com>
2020-10-03 20:15:13 +0200

    clean more of demonstrator

--- app/controllers/demonstrator.js
+++ app/controllers/demonstrator.js
@@ -1,26 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  didReceiveAttrs: function() {
-    this.set('selectedEvent', null);
-    return this.set('currentView', null);
-  },
   subMenu: Ember.computed('model', 'model.[]', function() {
     return this.get('model').map(eventTemplate => ({
-      key: eventTemplate,
       id: eventTemplate.get('id'),
       route: 'demonstrator.detail',
       description: eventTemplate.get('name')
     }));
-  }),
-  actions: {
-    cancel: function() {
-      this.set('selectedEvent', null);
-      return false;
-    },
-    evaluateEvent: function(event) {
-      this.set('selectedEvent', event);
-      return false;
-    }
-  }
+  })
 });

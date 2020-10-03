79c881957fa78751a66212636f26fff289c338a0
Szepes Nóra <nora.szepes@tenforce.com>
2020-10-04 01:35:51 +0200

    separate second attempts per group

--- app/components/demonstrator-group-data.js
+++ app/components/demonstrator-group-data.js
@@ -5,34 +5,45 @@ export default Ember.Component.extend({
   session: Ember.inject.service('session'),
   classNames: ['demonstrator-group'],
 
-  header: ['Név', 'Neptun', 'Feladattípus', 'Feltöltés ideje', 'Beugró érdemjegy', 'Beadandó érdemjegy', 'Labor érdemjegy', 'Pót'],
-  rowIndecies: ['StudentRegistration.User.displayName', 'StudentRegistration.User.neptun', 'ExerciseSheet.ExerciseType.shortName', 'firstCorrectableDeliverable.formattedLastSubmittedDate', 'firstEntryTest.grade', 'firstCorrectableDeliverable.grade', 'grade', 'supplementary'],
+  header: ['Név', 'Neptun', 'Feladattípus', 'Feltöltés ideje', 'Beugró érdemjegy', 'Beadandó érdemjegy', 'Labor érdemjegy'],
+  rowIndecies: ['StudentRegistration.User.displayName', 'StudentRegistration.User.neptun', 'ExerciseSheet.ExerciseType.shortName', 'firstCorrectableDeliverable.formattedLastSubmittedDate', 'firstEntryTest.grade', 'firstCorrectableDeliverable.grade', 'grade'],
 
-  sortedEvents: Ember.computed('events', 'events.[]', function() {
-    return this.get('events')
-      .sort((lhs, rhs) => {
-        const lhsName = lhs.get('StudentRegistration.User.displayName'), rhsName = rhs.get('StudentRegistration.User.displayName');
-        const lhsAttempt = lhs.get('attempt'), rhsAttempt = rhs.get('attempt');
-
-        if (lhsAttempt > rhsAttempt) {
-          return -1;
-        }
-
-        if (lhsAttempt < rhsAttempt) {
-          return 1;
-        }
-
-        if (lhsAttempt === rhsAttempt) {
-          if (lhsName < rhsName) {
-            return -1;
-          }
-          if (lhsName > rhsName) {
-            return 1;
-          }
-        }
-        return 0;
+  firstAttemptEvents: Ember.computed('events', 'events.[]', function() {
+    return this.get('events').filter(function(event) {
+        return event.get('attempt') !== 2;
       })
+      .sort(this.sortFunction)
   }),
+
+  secondAttemptEvents: Ember.computed('events', 'events.[]', function() {
+    return this.get('events').filter(function(event) {
+        return event.get('attempt') > 1;
+      })
+      .sort(this.sortFunction)
+  }),
+
+  sortFunction(lhs, rhs) {
+    const lhsName = lhs.get('StudentRegistration.User.displayName'), rhsName = rhs.get('StudentRegistration.User.displayName');
+    const lhsAttempt = lhs.get('attempt'), rhsAttempt = rhs.get('attempt');
+
+    if (lhsAttempt > rhsAttempt) {
+      return -1;
+    }
+
+    if (lhsAttempt < rhsAttempt) {
+      return 1;
+    }
+
+    if (lhsAttempt === rhsAttempt) {
+      if (lhsName < rhsName) {
+        return -1;
+      }
+      if (lhsName > rhsName) {
+        return 1;
+      }
+    }
+    return 0;
+  },
 
   eventGroup: Ember.computed('events', function() {
     return this.get('events.firstObject.CourseCode');

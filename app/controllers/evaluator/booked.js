2cb450b2e629be61ac29fb8886d89a1d9bf65f83
Szepes Nóra <nora.szepes@tenforce.com>
2020-11-02 17:12:05 +0100

    rework booked model and route

--- app/controllers/evaluator/booked.js
+++ app/controllers/evaluator/booked.js
@@ -2,46 +2,35 @@ import Ember from 'ember';
 import RSVP from 'rsvp';
 
 export default Ember.Controller.extend({
-    separatedDeliverablesByExerciseType: Ember.computed(function () {
-        return new RSVP.Promise((resolve, reject) => {
-          this.get('store').query('deliverable', {
-            filter: {
-              isCorrector: true,
-              isFile: true,
-              finalized: false
-            }
-          }).then(deliverables => {
-            let separatedDeliverablesByExerciseType = {}
-            let categoryById = []
+    separatedDeliverablesByExerciseType: Ember.computed('model', 'model.deliverables', function () {
+      const deliverables = this.get('model.deliverables');
+      let separatedDeliverablesByExerciseType = {};
+      let categoryById = [];
 
-            deliverables.map(deliverable => {
-              const exerciseCategory = deliverable.get('exerciseCategory');
-              const exerciseCategoryType = exerciseCategory.get('type');
-              if(!Object.keys(separatedDeliverablesByExerciseType).includes(exerciseCategoryType)) {
-                separatedDeliverablesByExerciseType[exerciseCategoryType] = [];
-                categoryById.push({ type: exerciseCategoryType, id: parseInt(exerciseCategory.get('id'))});
-              }
+      deliverables.map(deliverable => {
+        const exerciseCategory = deliverable.get('exerciseCategory');
+        const exerciseCategoryType = exerciseCategory.get('type');
+        if(!Object.keys(separatedDeliverablesByExerciseType).includes(exerciseCategoryType)) {
+          separatedDeliverablesByExerciseType[exerciseCategoryType] = [];
+          categoryById.push({ type: exerciseCategoryType, id: parseInt(exerciseCategory.get('id'))});
+        }
 
-              separatedDeliverablesByExerciseType[exerciseCategoryType].push(deliverable);
-              return deliverable;
-            });
+        separatedDeliverablesByExerciseType[exerciseCategoryType].push(deliverable);
+        return deliverable;
+      });
 
-            const sortedCategories = categoryById.sort(function(a, b) {
-              return a.id - b.id;
-            })
+      const sortedCategories = categoryById.sort(function(a, b) {
+        return a.id - b.id;
+      })
 
-            let finalDeliverablesByExerciseType = {}
-            sortedCategories.forEach(function(sorted) {
-              finalDeliverablesByExerciseType[sorted.type] = separatedDeliverablesByExerciseType[sorted.type];
-            });
+      let finalDeliverablesByExerciseType = {}
+      sortedCategories.forEach(function(sorted) {
+        finalDeliverablesByExerciseType[sorted.type] = separatedDeliverablesByExerciseType[sorted.type];
+      });
 
-            resolve(finalDeliverablesByExerciseType);
-          }, err => {
-            console.error(err);
-            reject(err);
-          });
-        });
-      }),
+      return finalDeliverablesByExerciseType;
+
+    }),
 
     actions: {
       changeDeliverable(deliverable) {

4611257a61e9b5acf0b95e0a69f374697240133b
Szepes Nóra <nora.szepes@tenforce.com>
2020-11-02 17:13:25 +0100

    rework fixed model and route

--- app/routes/evaluator/fixed.js
+++ app/routes/evaluator/fixed.js
@@ -1,16 +1,19 @@
 import Ember from 'ember';
-import RSVP from 'rsvp';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ErrorRouteMixin from '../../mixins/error-route';
 import CorrectorAuthenticatedRouteMixin from '../../mixins/corrector-authenticated-route';
-import jwt_decode from 'npm:jwt-decode';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, ErrorRouteMixin, CorrectorAuthenticatedRouteMixin, {
   model() {
-    var token = this.get('session.data.authenticated.token');
-    var userData = jwt_decode(token);
     return RSVP.hash({
-      user: this.get('store').find('user', userData.userId),
+      deliverables: this.get('store').query('deliverable', {
+        filter: {
+          isAttached: true,
+          isFile: true,
+          finalized: true
+        }
+      }),
       eventTemplates: this.get('store').query('eventTemplate', {
         filter: {
           asCorrector: true

c96304912dc7f9ae6ed9dac2bb1b85737ca99543
Mark Fejes <fejes.mark92@gmail.com>
2025-10-14 10:51:15 +0200

    fix: logout the user when jwt token expired

--- app/authenticators/shibboleth.js
+++ app/authenticators/shibboleth.js
@@ -1,12 +1,41 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 import config from '../config/environment';
+import jwtDecode from 'npm:jwt-decode';
 
 export default Base.extend({
   ajax: Ember.inject.service(),
+  session: Ember.inject.service(),
+
+  _setupTokenExpiration(token) {
+    try {
+      const decodedToken = jwtDecode(token);
+      if (decodedToken && decodedToken.exp) {
+        const expiresIn = decodedToken.exp - Math.floor(Date.now() / 1000);
+        if (expiresIn > 0) {
+          setTimeout(() => {
+            const session = this.get('session');
+            if (session.isAuthenticated) {
+              session.invalidate().then(() => {
+                const router = Ember.getOwner(this).lookup('router:main');
+                router.transitionTo('login');
+              });
+            }
+          }, expiresIn * 1000);
+        }
+      }
+    } catch (error) {
+      console.error('Error decoding JWT token:', error);
+    }
+  },
 
   authenticate() {
-    return this.get('ajax').request(config['ember-simple-auth-token'].serverTokenShibbolethEndpoint);
+    return this.get('ajax').request(config['ember-simple-auth-token'].serverTokenShibbolethEndpoint).then((response) => {
+      if (response && response.token) {
+        this._setupTokenExpiration(response.token);
+      }
+      return response;
+    });
   },
 
   restore(data) {
@@ -18,6 +47,7 @@ export default Base.extend({
           method: 'POST',
           data: { token: dataObject.get('token') }
         }).then((response) => {
+          this._setupTokenExpiration(dataObject.get('token'));
           resolve(data);
         }).catch(() => {
           reject();
